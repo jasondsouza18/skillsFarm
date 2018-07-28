265f54297af42226c515c74d12a70d8d98aef6cf
jason <jasondsouza717@gmail.com>
2018-07-28 15:36:44 +0530

    added images, fonts, and cssjs

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+class HomeController extends Controller
+{
+    /**
+     * @Route("/home", name="home")
+     */
+    public function index()
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    /**
+     * @Route("/contactus", name="contactus")
+     */
+    public function contactus()
+    {
+        echo "hi";
+        die;
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+}
