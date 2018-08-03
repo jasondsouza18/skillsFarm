c38222f827c763aa13208e0116d1bfa58bfc868f
Jason Vinod Dsouza <jason.vinod@techjini.com>
2018-08-03 20:34:27 +0530

    changes on mailer

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -21,8 +21,34 @@ class HomeController extends Controller
     /**
      * @Route("/contactus", name="contactus")
      */
-    public function contactus()
+    public function contactus(\Swift_Mailer $mailer)
     {
+        $name = 'jason';
+        $message = (new \Swift_Message('Hello Email'))
+        ->setFrom('jason.vinod@techjini.com')
+        ->setTo('jason.vinod@techjini.com')
+        ->setBody(
+            $this->renderView(
+                // templates/emails/registration.html.twig
+                'base.html.twig',
+                array('name' => $name)
+            ),
+            'text/html'
+        )
+        /*
+         * If you also want to include a plaintext version of the message
+        ->addPart(
+            $this->renderView(
+                'emails/registration.txt.twig',
+                array('name' => $name)
+            ),
+            'text/plain'
+        )
+        */
+    ;
+
+     echo  $mailer->send($message);die;
+
         echo "hi";
         die;
         return $this->render('home/index.html.twig', [
