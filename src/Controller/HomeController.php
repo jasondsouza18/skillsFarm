9d4ca00252451362468b402f0721fefdf2f9cd26
jason <jasondsouza717@gmail.com>
2018-08-18 20:47:18 +0530

    changes in form and toast mesage display@

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,7 +9,6 @@ use Symfony\Component\HttpFoundation\Request;
 use App\Form\ContactUsType;
 
 
-
 class HomeController extends Controller
 {
     /**
@@ -30,7 +29,7 @@ class HomeController extends Controller
     /**
      * @Route("/contactus", name="_contactus")
      */
-    public function contactus(Request $request,\Swift_Mailer $mailer)
+    public function contactus(Request $request, \Swift_Mailer $mailer)
     {
         $form = $this->createForm(ContactUsType::class);
         $form->handleRequest($request);
@@ -41,8 +40,8 @@ class HomeController extends Controller
                 ->setFrom('skillsfarmindia@gmail.com')
                 ->setTo('jasondsouza717@gmail.com')
                 ->setCc('josephjeffry2@gmail.com')
-                ->setBody("Name - ".$data['vc_name'].PHP_EOL."Email - ".$data['vc_email'].PHP_EOL
-                    ."Subject -".$data['vc_subject'].PHP_EOL." Message - ".PHP_EOL.$data['vc_message'].PHP_EOL
+                ->setBody("Name - " . $data['vc_name'] . PHP_EOL . "Email - " . $data['vc_email'] . PHP_EOL
+                    . "Subject -" . $data['vc_subject'] . PHP_EOL . " Message - " . PHP_EOL . $data['vc_message'] . PHP_EOL
                 );
             $mailer->send($message);
         }
@@ -62,8 +61,9 @@ class HomeController extends Controller
     /**
      * @Route("/general", name="_general")
      */
-    public function general(Request $request,\Swift_Mailer $mailer)
+    public function general(Request $request, \Swift_Mailer $mailer)
     {
+        $sent = 0;
         if ($request->getMethod() == 'POST') {
             $projectRoot = $this->get('kernel')->getProjectDir();
             $uploadsDirectory = $projectRoot . "/public/uploads/general/Resumes/";
@@ -71,22 +71,24 @@ class HomeController extends Controller
             $fileName = $file->getClientOriginalName();
             $file->move($uploadsDirectory, $fileName);
             $request = $request->request->all();
-            $message = " SkillsFarm Update".PHP_EOL."Name = ".$request['name'].PHP_EOL.
-                        "Email = ".$request['Email'].PHP_EOL.
-                        "Phone number = " .$request['number'].PHP_EOL.
-                        "stream = ".$request['Stream'].PHP_EOL.
-                        "place = ".$request['Place'].PHP_EOL.
-                        "website = ".$request['website'].PHP_EOL.
-                        "cover letter  = ".$request['cover'].PHP_EOL.
-                        "Resume name = ".$fileName;
+            $message = " SkillsFarm Update" . PHP_EOL . "Name = " . $request['name'] . PHP_EOL .
+                "Email = " . $request['Email'] . PHP_EOL .
+                "Phone number = " . $request['number'] . PHP_EOL .
+                "stream = " . $request['Stream'] . PHP_EOL .
+                "place = " . $request['Place'] . PHP_EOL .
+                "website = " . $request['website'] . PHP_EOL .
+                "cover letter  = " . $request['cover'] . PHP_EOL .
+                "Resume name = " . $fileName;
             $messagetosend = (new \Swift_Message('Skills Farm'))
                 ->setFrom('skillsfarmindia@gmail.com')
                 ->setTo('jasondsouza717@gmail.com')
                 ->setCc('josephjeffry2@gmail.com')
                 ->setBody($message)
-                ->attach(\Swift_Attachment::fromPath($uploadsDirectory.$fileName));
-            echo $mailer->send($messagetosend);
+                ->attach(\Swift_Attachment::fromPath($uploadsDirectory . $fileName));
+            $sent= $mailer->send($messagetosend);
         }
-        return $this->render('home/generalform.html.twig');
+        return $this->render('home/generalform.html.twig', array(
+            'sent' => $sent
+        ));
     }
 }
